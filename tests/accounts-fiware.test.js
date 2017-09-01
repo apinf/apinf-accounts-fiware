ef33b6e96fd50374ba2bca9d2aaf659114b2fd4f
yannunes <yannunes@gmail.com>
2017-08-31 21:43:36 -0300

    Add unit test file for both server and client

---
+++ tests/accounts-fiware.test.js
@@ -0,0 +1,12 @@
+
+import { expect } from 'meteor/practicalmeteor:chai'
+
+if (!Meteor.isClient) {
+
+} else {
+  describe('first test', function() {
+    it('should pass', function(done) {
+      done()
+    })
+  })
+}
